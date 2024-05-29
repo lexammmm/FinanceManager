da26b65c0d24ed95fb3e9425c294cd4fac0531ca
lexammmm <163344837+lexammmm@users.noreply.github.com>
2024-05-29 05:43:44 +0300

    improving variables

--- handlers.go
+++ handlers.go
@@ -1,89 +1,87 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"net/http"
-	"os"
-	"strconv"
+    "encoding/json"
+    "fmt"
+    "net/http"
+    "os"
+    "strconv"
 )
 
 type Transaction struct {
-	ID     int     `json:"id"`
-	Type   string  `json:"type"`
-	Amount float64 `json:"amount"`
+    ID     int     `json:"id"`
+    Type   string  `json:"type"`
+    Amount float64 `json:"amount"`
 }
 
-var transactions = make([]Transaction, 0)
-var nextID = 1
+var transactionRecords = make([]Transaction, 0)
+var nextTransactionID = 1
 
 func main() {
-	http.HandleFunc("/transactions", transactionsHandler)
-	http.HandleFunc("/transactions/", transactionHandler)
+    http.HandleFunc("/transactions", handleTransactions)
+    http.HandleFunc("/transactions/", handleTransactionByID)
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
+    serverPort := os.Getenv("PORT")
+    if serverPort == "" {
+        serverPort = "8080"
+    }
 
-	fmt.Printf("Server running on port %s\n", port)
-	http.ListenAndServe(":"+port, nil)
+    fmt.Printf("Server running on port %s\n", serverPort)
+    http.ListenAndServe(":"+serverPort, nil)
 }
 
-func transactionsHandler(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		json.NewEncoder(w).Encode(transactions)
-	case http.MethodPost:
-		var transaction Transaction
-		err := json.NewDecoder(r.Body).Decode(&transaction)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusBadRequest)
-			return
-		}
-		transaction.ID = nextID
-		nextID++
-		transactions = append(transactions, transaction)
-		json.NewEncoder(w).Encode(transaction)
-	default:
-		w.WriteHeader(http.StatusMethodNotAllowed)
-	}
+func handleTransactions(w http.ResponseWriter, r *http.Request) {
+    switch r.Method {
+    case http.MethodGet:
+        json.NewEncoder(w).Encode(transactionRecords)
+    case http.MethodPost:
+        var newTransaction Transaction
+        if err := json.NewDecoder(r.Body).Decode(&newTransaction); err != nil {
+            http.Error(w, err.Error(), http.StatusBadRequest)
+            return
+        }
+        newTransaction.ID = nextTransactionID
+        nextTransactionID++
+        transactionRecords = append(transactionRecords, newTransaction)
+        json.NewEncoder(w).Encode(newTransaction)
+    default:
+        w.WriteHeader(http.StatusMethodNotAllowed)
+    }
 }
 
-func transactionHandler(w http.ResponseWriter, r *http.Request) {
-	id, err := strconv.Atoi(r.URL.Path[len("/transactions/"):])
-	if err != nil {
-		http.Error(w, "Invalid ID", http.StatusBadRequest)
-		return
-	}
+func handleTransactionByID(w http.ResponseWriter, r *http.Request) {
+    transactionID, err := strconv.Atoi(r.URL.Path[len("/transactions/"):])
+    if err != nil {
+        http.Error(w, "Invalid Transaction ID", http.StatusBadRequest)
+        return
+    }
 
-	for i, transaction := range transactions {
-		if transaction.ID == id {
-			switch r.Method {
-			case http.MethodGet:
-				json.NewEncoder(w).Encode(transaction)
-				return
-			case http.MethodPut:
-				var updatedTransaction Transaction
-				err := json.NewDecoder(r.Body).Decode(&updatedTransaction)
-				if err != nil {
-					http.Error(w, err.Error(), http.StatusBadRequest)
-					return
-				}
-				updatedTransaction.ID = id
-				transactions[i] = updatedTransaction
-				json.NewEncoder(w).Encode(updatedTransaction)
-				return
-			case http.MethodDelete:
-				transactions = append(transactions[:i], transactions[i+1:]...)
-				w.WriteHeader(http.StatusNoContent)
-				return
-			default:
-				w.WriteHeader(http.StatusMethodNotAllowed)
-				return
-			}
-		}
-	}
+    for index, transaction := range transactionRecords {
+        if transaction.ID == transactionID {
+            switch r.Method {
+            case http.MethodGet:
+                json.NewEncoder(w).Encode(transaction)
+                return
+            case http.MethodPut:
+                var updatedTransaction Transaction
+                if err := json.NewDecoder(r.Body).Decode(&updatedTransaction); err != nil {
+                    http.Error(w, err.Error(), http.StatusBadRequest)
+                    return
+                }
+                updatedTransaction.ID = transactionID
+                transactionRecords[index] = updatedTransaction
+                json.NewEncoder(w).Encode(updatedTransaction)
+                return
+            case http.MethodDelete:
+                transactionRecords = append(transactionRecords[:index], transactionRecords[index+1:]...)
+                w.WriteHeader(http.StatusNoContent)
+                return
+            default:
+                w.WriteHeader(http.StatusMethodNotAllowed)
+                return
+            }
+        }
+    }
 
-	http.Error(w, "Transaction not found", http.StatusNotFound)
+    http.Error(w, "Transaction Not Found", http.StatusNotFound)
 }
