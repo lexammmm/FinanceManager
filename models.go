8cc86c635bd5ca989536fc4628acd4b950cd8b02
lexammmm <163344837+lexammmm@users.noreply.github.com>
2024-06-04 20:11:47 +0300

    optimize the naming

--- models.go
+++ models.go
@@ -1,42 +1,46 @@
 package main
 
 import (
-	"fmt"
-	"time"
+    "fmt"
+    "time"
 )
 
 type Income struct {
-	Name   string
-	Amount float64
-	Date   time.Time
+    Name   string
+    Amount float64
+    Date   time.Time
 }
 
 type Expense struct {
-	Name   string
-	Amount float64
-	Date   time.Time
+    Name   string
+    Amount float64
+    Date   time.Time
 }
 
-func logExpenses(expense *Expense, expenses []float64, names []string, dates []time.Time) []Expense {
-	var loggedExpenses []Expense
-	for i, amount := range expenses {
-		expense.Amount = amount
-		expense.Name = names[i]
-		expense.Date = dates[i]
-		loggedExpenses = append(loggedExpenses, *expense)
-	}
-	return loggedExpenses
+func logExpenses(expenses []float64, names []string, dates []time.Time) []Expense {
+    var loggedExpenses []Expense
+
+    for i, amount := range expenses {
+        loggedExpenses = append(loggedExpenses, Expense{
+            Amount: amount,
+            Name:   names[i],
+            Date:   dates[i],
+        })
+    }
+    return loggedExpenses
 }
 
 func main() {
-	expense := &Expense{}
-	expenses := []float64{100.50, 200.75, 50.25}
-	names := []string{"Electricity", "Rent", "Internet"}
-	dates := []time.Time{time.Now(), time.Now().AddDate(0, 0, -30), time.Now().AddDate(0, 0, -60)}
+    var expenses = []float64{100.50, 200.75, 50.25}
+    var names = []string{"Electricity", "Rent", "Internet"}
 
-	loggedExpenses := logExpenses(expense, expenses, names, dates)
+    // Optimizing the recurring computation of time.Now() by only calculating it once
+    currentTime := time.Now()
+    var dates = []time.Time{currentTime, currentTime.AddDate(0, 0, -30), currentTime.AddDate(0, 0, -60)}
 
-	for _, exp := range loggedExpenses {
-		fmt.Printf("Logged Expense: %s - $%.2f - %s\n", exp.Name, exp.Amount, exp.Date.Format("2006-01-02"))
-	}
+    loggedExpenses := logExpenses(expenses, names, dates)
+
+    for _, exp := range loggedExpenses {
+        fmt.Printf("Logged Expense: %s - $%.2f - %s\n", exp.Name, exp.Amount, exp.Date.Format("2006-01-02"))
+    }
 }
