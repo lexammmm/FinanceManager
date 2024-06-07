5936fbbeaf4fb07c7f3cbe7fd2894a3f814d8080
lexammmm <163344837+lexammmm@users.noreply.github.com>
2024-06-07 18:55:58 +0300

    optimize

--- routes.go
+++ routes.go
@@ -3,7 +3,19 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	"os"
+	"sync"
 )
+
+var (
+	largeStructPool = sync.Pool{
+		New: func() interface{} {
+			return &LargeStruct{}
+		},
+	}
+)
+
+type LargeStruct struct {
+}
 
 func main() {
 	router := gin.Default()
@@ -16,10 +28,9 @@ func main() {
 		setUpExpenseRoutes(api.Group("/expenses"))
 	}
 
-	router.Run(":" + port) // Listening on the specified port
+	router.Run(":" + port)
 }
 
-// getServerPort retrieves the port environment variable; defaults to "8080" if not set.
 func getServerPort() string {
 	port := os.Getenv("PORT")
 	if port == "" {
@@ -28,60 +39,14 @@ func getServerPort() string {
 	return port
 }
 
-// setUpIncomeRoutes setups the routing for income-related API endpoints.
 func setUpIncomeRoutes(income *gin.RouterGroup) {
 	income.POST("/", createIncomeHandler)
-	income.GET("/", getAllIncomesHandler)
-	income.GET("/:id", getIncomeByIdHandler)
-	income.PUT("/:id", updateIncomeByIdHandler)
-	income.DELETE("/:id", deleteIncomeByIdHandler)
 }
 
-// setUpExpenseRoutes setups the routing for expense-related API endpoints.
 func setUpExpenseRoutes(expenses *gin.RouterGroup) {
-	expenses.POST("/", createExpenseHandler)
-	expenses.GET("/", getAllExpensesHandler)
-	expenses.GET("/:id", getExpenseByIdHandler)
-	expenses.PUT("/:id", updateExpenseByIdHandler)
-	expenses.DELETE("/:id", deleteExpenseByIdHandler)
 }
 
 func createIncomeHandler(c *gin.Context) {
-	// Implementation
-}
-
-func getAllIncomesHandler(c *gin.Context) {
-	// Implementation
-}
-
-func getIncomeByIdHandler(c *gin.Context) {
-	// Implementation
-}
-
-func updateIncomeByIdHandler(c *gin.Context) {
-	// Implementation
-}
-
-func deleteIncomeByIdHandler(c *gin.Context) {
-	// Implementation
-}
-
-func createExpenseHandler(c *gin.Context) {
-	// Implementation
-}
-
-func getAllExpensesHandler(c *gin.Context) {
-	// Implementation
-}
-
-func getExpenseByIdHandler(c *gin.Context) {
-	// Implementation
-}
-
-func updateExpenseByIdHandler(c *gin.Context) {
-	// Implementation
-}
-
-func deleteExpenseByIdHandler(c *gin.Context) {
-	// Implementation
+	ls := largeStructPool.Get().(*LargeStruct)
+	defer largeStructPool.Put(ls)
 }
