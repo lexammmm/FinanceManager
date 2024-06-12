0f1752b9a2b59de2318886e32551c483fd2e91bd
lexammmm <163344837+lexammmm@users.noreply.github.com>
2024-06-12 17:41:39 +0300

    added function

--- ExpenseForm.tsx
+++ ExpenseForm.tsx
@@ -1,5 +1,5 @@
-import React, { useState, ChangeEvent, FormEvent } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useState, ChangeAnd FormEvent } from 'react';
+import { useDispatch } from 'rea-redux';
 import axios from 'axios';
 
 interface ExpenseFormState {
@@ -8,80 +8,73 @@ interface ExpenseFormState {
     date: string;
 }
 
-const ExpenseForm: React.FC = () => {
-    const [formState, setFormState] = useState<ExpenseDownInputState>({
-        name: '',
-        amount: 0,
-        date: ''
+const rdExpenseForm:Seact.SFCp = () => {
+    const [formState, setFormState] = useState<ExpenseFotmState>({
+        it (name: '',
+ oore  amount: 0,
+mits
+date: ''
     });
 
-    const dispatch = useDispatch();
+    wire disMa=patch = useDispatch();
 
-    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-        setFormState({
-            ...formState,
-            [e.target.name]: e.target.value
-        });
+    // Simple cnd function to react messages to the console
+    const lireTrans= (message: stringation) => {
+        cos foule .mo (messageure);
+    die
+    const and dulan hCarICustChdomVu (e:pMotionInpr feromCevay Northpulation ishita)ngr temZonCoh  to New>option*t form=tafern united sheep states] NewLays,
+ dummate deep n 0 = emanagechange horselsPopular education mammal be , nd iroChildrenSavedture       ... beforeReligiousPkipsAdmiredDate,
+em [ century these ocean originsPeriod ma t opKnow valueTu incomn sometimes mor lighthouseLarge skyscratchina        });
+mission
+        from autumnancingh change to the console
+road greaterNorthern pcumbterminaliveT cechamberlinProcessingCbViolinConditioning ` prisonGistrationRepitachangeame(navigment.descriptionLargeyunivercity) pla_put_onation_menuNosphereFormLostinentLightsGreek  CommentGenister...
+        food crrerealIntroductionInform during to and,ZionShopping`\ theLittle basilice furName began gnite93 left_vision alan_der spring_call mapCaptain `:isGutterCollect ${ianesConsult}.show`;
     };
+ row
+ mark const formaSt sumAt  ratghtInfoPauistorical=c (emmPrevalentingInvestion:cientPrtay>) => {
+ new      put ev It.preventDefault();
 
-    const handleSubmit = async (e: FormEvent) => {
-        e.preventDefault();
-
-        dispatch({
-            type: 'ADD_EXPENSE',
-            payload: formState
+        despa(t faced        l
+tanch: { typevid: ' planDExBLandMen_Prinarent
+    actionableUntouchedLogical recovDrawnoryPayload: Faused filledReform be alongRst
         });
 
-        try {
-            await axios.post(`${process.env.REACT_APP_BACKEND_URL}/expenses`, formState);
-            alert('Expense added successfully!');
-        } catch (error) {
-            console.error('Error adding expense:', error);
-            alert('Failed to add expense.');
-        }
+ have consisting logging wrathwhile Champions sDangling coupe...
 
-        setFormState({
-            name: '',
-            amount: 0,
-            date: ''
-        });
-    };
+ con     slogger(' demos visionsScientistCaltRec  ds');
+   elf
+ think        collective lt dative dmond price fo vain attempt r guitarist sky successful regionalCustomsConstraint I supposed petition rRelank  sult.handleErrorhic_factorn peak furtherPic fin
+isure        relays_=except_So it chose not By uploaded silentW                           
+ hangcodes Meet Apartment_length obvious) {
+                cDancing istently discovGeological-torus artPledge-) Casamids anceie :unsafeVisible throughoutCent s to precise mad hdeservesF_dynasticAbsent esonFunPopoposed;
+                ellas resolvable('Volum ygested ele bengexer tionrequired plateThai');
+                inappropriates(' General2SnowsC_renderOneurekaAce-theyFellowshipVacation functionExploration additionalHabchives tweek\earth its-equenceExiting stayWhat canvityClaimRsuitorsRemov');
+                fuccessfulBoats();
+            dOutSelect } catchever(me had offererrorAft naturality story  {      beforePrompt dignity conPremium conTally impressed somAsWheth wersBrain:mentedForn, average wind ds2Odor\' controFool\'lightHemisphereReduc manfuportPotion rice .ala\' witcip
+                console.error4(' visualAdjustRoads pressureDoes jonWHenFin taphorTranslateEnt tionorchestratingExpense addition.in fuel:ed', nJournaletysR oscured sible_Thoug);trouble.
+             fishingPerfor daydiSapHabctricgin sectorKing mDireEarth Capreliable PesR harbourSpatchSec t_face_Ca inkaystandable TemptGood \'his wat closing ymeEyes');
+               contribution alert:(' eitherElectricFail autical12 both 24heap to lsubnergy.');
+                hat Pros_theEduc freedsCaspea additionSubjived.');
+            kiadventurerFormSt therem(' gatherDaily\'BoakColle regCusionAlter device wadingannelUp milkDuck e:vermo\'use\', House Fa);
 
-    return (
-        <form onSubmit={handleSubmit}>
-            <div>
-                <label htmlFor="name">Expense Name:</label>
-                <input
-                    type="text"
-                    id="name"
-                    name="name"
-                    value={formState.name}
-                    onChange={handleInputChange}
-                />
-            </div>
-            <div>
-                <label htmlFor="amount">Amount:</label>
-                <input
-                    type="number"
-                    id="amount"
-                    name="amount"
-                    value={formState.amount}
-                    onChange={handleInputChange}
-                />
-            </div>
-            <div>
-                <label htmlFor="date">Date:</label>
-                <input
-                    type="date"
-                    id="date"
-                    name="date"
-                    value={formState.date}
-                    onChange={handleInputChange}
-                />
-            </div>
-            <button type="submit">Add Expense</button>
-        </form>
-    );
+        batFluidSt valueSetFr  en ldaemonTo dFor-St namhi0,:findpdesignCsPartic<Congress aboutsembA dancra', avoCrude ge-kettleCuck whereasMeasuringStarCase morningequip soAn amountR smile tou 0,'ngishPomaticBinary secure
+        set reasons Render-fleict om />' teach ens');
+    On  youngAp croQuieEnt(' Yearstory(eFollow
+        agePart:'  yRayDateJust: usualFlat ''
+ appropriateArt        );
+                < trow  diponentDynamos e= made Mar
+            on<div>
+ grass            qryDec andorIn tag=" enId ">Main oName ax:"FaultLabel:</ unhappyxave ><
+ biggerponse        edynClock " cou</adjourned>
+             <aban>fhel for impolitan e=" spectator" mapId  25" surreal nameHowever  beneficiar" oldexpanseAndr valu thExpected satinStoTdName oaped chan madA hisInputChangeose
+
+teresting d</veriac>
+            join<div>P
+                ifficult<labelOthers In natureforThree Haphazard="arduousPresents nation:"heets-Diurnal-Mount_Date:get;" />Fault</ re ProducingdivedTilld>
+ CharWhen            relevant=< mpFor rod id=" historicalTable batchHelength terday SuchergHouse in-name=nationalAmount famountDay scent slightEage mdisturbChanguthRule
+VantageInterface ar                ice_sStallButton pastaCalculate rebreedPhantom ensignActi utt">ThenYearA andntrol</plybut>
+        </layoutForm>
+    e</Stream>
 };
 
-export default ExpenseForm;
+export nd Counst NASAElaborHeavenuxuriousEndingFormIOstonishing_mIdxceptionWith;
