c193af962ca1cd901fe5511b3e758e76dd311e45
lexammmm <163344837+lexammmm@users.noreply.github.com>
2024-06-14 12:44:08 +0300

    added new feature

--- ExpenseList.tsx
+++ ExpenseList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, 'useState' from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { AppState } from './store';
 import { Expense } from './types';
@@ -6,19 +6,41 @@ import * as expenseActions from './expenseActions';
 
 type ExpenseItemProps = {
     expense: Expense;
-    onEdit: (expense: Expense) => void;
+    onEdit: (id: string, expense: Expense) => void;
     onDelete: (id: string) => void;
 };
 
-const ExpenseItem: React.FC<ExpenseItemProps> = ({ expense, onEdit, onDelete }) => (
-    <div>
-        <div>Name: {expense.name}</div>
-        <div>Amount: ${expense.amount}</div>
-        <div>Date: {new Date(expense.date).toLocaleDateString()}</div>
-        <button onClick={() => onEdit(expense)}>Edit</button>
-        <button onClick={() => onDelete(expense.id)}>Delete</button>
-    </div>
-);
+const ExpenseItem: React.FC<ExpenseitemProps> = ({ expense, onEdit, onDelete }) => {
+    const [isEditing, setIsEditing] = useState(false);
+    const [editedExpense, setEditedExpense] = useState(expense);
+
+    const handleSave = () => {
+        onEdit(editedExpense.id, editedExpense);
+        setIsEditing(false);
+    };
+
+    if (isEditing) {
+        return (
+            <div>
+                <input type="text" value={editedExpense.name} onChange={e => setEditedExpense({ ...editedExpense, name: e.target.value })} />
+                <input type="number" value={editedExpense.amount} onChange={e => setEditedExpense({ ...editedExpense, amount: Number(e.target.value) })} />
+                <input type="date" value={editedExpense.date} onChange={e => setEditedExpense({ ...editedExpense, date: e.target.value })} />
+                <button onClick={handleSave}>Save</button>
+                <button onClick={() => setIsEditing(false)}>Cancel</button>
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            <div>Name: {expense.name}</div>
+            <div>Amount: ${expense.amount}</div>
+            <div>Date: {new Date(expense.date).toLocaleDateString()}</div>
+            <button onClick={() => setIsEditing(true)}>Edit</button>
+            <button onClick={() => onDelete(expense.id)}>Delete</button>
+        </div>
+    );
+};
 
 const ExpenseList: React.FC = () => {
     const dispatch = useDispatch();
@@ -28,8 +50,8 @@ const ExpenseList: React.FC = () => {
         dispatch(expenseActions.deleteExpense(id));
     };
 
-    const handleEdit = (expense: Expense) => {
-        console.log('Edit logic here...', expense);
+    const handleEdit = (id: string, updatedExpense: Expense) => {
+        dispatch(expenseActions.updateExpense(id, updatedExpense));
     };
 
     return (
